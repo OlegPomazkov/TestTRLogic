b817f078c0f2f27346cea842819443e7d706a1d6
OlegPomazkov <povcode@gmail.com>
2017-11-10 12:03:23 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <my-row :rowData ='rowData' v-on:valueChanged="changeInput" ></my-row>
+  </div>
+</template>
+
+<script>
+import row from './SimpleRow.vue'
+
+export default {	
+  components: {
+    'my-row': row
+  }, 
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      rowData: {
+      	a: 'some name'
+      }
+    }
+  },
+
+  methods: {
+    changeInput: function (obj) {
+      this.msg = obj.newValue
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
